338c9ddb46089afaacec2bd5e0173f8dedfe3449
Joilr <colamanx@protonmail.com>
2023-12-09 19:21:26 +0100

    feedback design and code

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,49 +1,73 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ name, exercises }) => <p>{name} {exercises}</p>;
-
-const Content = ({ parts }) => (
-  <div>
-    <Part name={parts[0].name} exercises={parts[0].exercises} />
-    <Part name={parts[1].name} exercises={parts[1].exercises} />
-    <Part name={parts[2].name} exercises={parts[2].exercises} />
-  </div>
-);
+import { useState } from 'react'
+import './App.css';
 
 
-const Total = ({ parts }) => (
-  <p>
-    Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-  </p>
-);
 
 const App = () => {
-  
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const increaseGood = () => {
+    const newGood = good + 1;
+    const newTotal = total + 1;
+    setGood(good + 1);
+    setTotal(total +1);
+    averageScore(newGood, neutral, bad, newTotal);
+    positiveScore(newGood, newTotal)
   }
+
+  const increaseNeutral = () => {
+    const newNeutral = neutral + 1;
+    const newTotal = total + 1;
+    setNeutral(neutral + 1);
+    setTotal(total +1);
+    averageScore(good, newNeutral, bad, newTotal);
+    positiveScore(good, newTotal);
+  }
+
+  const increaseBad = () => {
+    const newBad = bad + 1;
+    const newTotal = total + 1;
+    setBad(bad + 1);
+    setTotal(total +1);
+    averageScore(good, neutral, newBad, newTotal);
+    positiveScore(good, newTotal);
+  }
+
+  const averageScore = (newGood, newNeutral, newBad, newTotal) => {
+    const newAverage = (newGood * 1 + newNeutral * 0 + newBad * -1) / newTotal;
+    setAverage(newAverage);
+  }
+
+  const positiveScore = (newGood, newTotal) => {
+    const newPositive = newTotal ? (newGood / newTotal) * 100 : 0;
+
+    setPositive(newPositive);
+};
+
+
+
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+       <h1 className="feedback-header">give feedback</h1>
+       <button onClick={increaseGood} className="feedback-buttons">good</button>
+       <button onClick={increaseNeutral} className="feedback-buttons">neutral</button>
+       <button onClick={increaseBad} className="feedback-buttons">bad</button>
+       <h2>statistics</h2>
+       <h3>good {good}</h3>
+       <h3>neutral {neutral}</h3>
+       <h3>bad {bad}</h3>
+       <h3>all {total}</h3>
+       <h3>average {average}</h3>
+       <h3>positive {positive} %</h3>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
