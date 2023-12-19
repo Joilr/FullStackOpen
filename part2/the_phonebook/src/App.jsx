f79c4e370836d918624638b95a324f6b7f50bca0
Joilr <colamanx@protonmail.com>
2023-12-19 23:29:46 +0100

    Exercises up to 2.16

--- part2/the_phonebook/src/App.jsx
+++ part2/the_phonebook/src/App.jsx
@@ -1,26 +1,133 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
 
+  //State handlers
+  const [persons, setPersons] = useState([]); 
+  const [newPerson, setNewPerson] = useState({ name: '', number: '' });
+  const [newFilter, setNewFilter] = useState('');
+
+  //Fetch data from the back-end (server)
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, []);
+
+  //Delete functionality
+  const deletePerson = ( id ) => {
+
+    personService
+    .removal(id)
+    .then(() => {
+      setPersons(persons.filter(person => person.id !== id));
+    })
+
+  };
+
+  //Person Form functionality
+  const addPerson = (event) => {
+
+    event.preventDefault();
+
+    //Person already exist in list
+    if (persons.some((person) => person.name === newPerson.name && person.number === newPerson.number)) {
+      alert(`${newPerson.name} is already added to phonebook with this number`);
+    }
+
+    //Changing phonenumber
+    else if (persons.some((person) => person.name === newPerson.name && person.number !== newPerson.number)) {
+      const confirmUpdate = window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`);
+
+      if (confirmUpdate) {
+
+        const personObject = {
+          name: newPerson.name,
+          number: newPerson.number,
+          id: newPerson.name
+        }
+
+        personService
+        .update(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.map(person => person.id !== personObject.id ? person : returnedPerson));
+        })
+      }
+    }
+
+    //Adding new person
+    else {
+    const personObject = {
+      name: newPerson.name,
+      number: newPerson.number,
+      id: newPerson.name
+    }
+
+    //Save data to the server
+    personService
+    .create(personObject)
+    .then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson))
+      setNewPerson({ name: '', number: '' }); 
+    })
+    }
+  };
+
+  //Person form input handler
+  const handleInputChange = (event) => {
+
+    const { name, value } = event.target;
+
+    setNewPerson(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  //Filter functionality
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  const filteredPersons = newFilter
+  ? persons.filter(
+    (person) =>
+      person.name.toLowerCase().includes(newFilter.toLowerCase()) ||
+      person.number.includes(newFilter)
+  )
+  : persons;
+
+  //The visual output
   return (
     <div>
+      
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+
+      <Filter value={newFilter} onChange={handleFilterChange} />
+      
+      <h3>add a new</h3>
+
+      <PersonForm 
+      newPerson={newPerson} 
+      handleInputChange={handleInputChange} 
+      addPerson={addPerson} 
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons 
+      filteredPersons={filteredPersons}
+      deletePerson={deletePerson}
+      />
+
     </div>
   )
-}
+};
 
-export default App
+export default App;
