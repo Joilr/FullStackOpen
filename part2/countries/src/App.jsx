74545c4bcbdb66e434ba1a22959ad4cd9ef56970
Joilr <colamanx@protonmail.com>
2023-12-20 23:34:08 +0100

    progress

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-//import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [countries, setCountry] = useState([
-    { name: 'Norway', id: 'Norway' },
-    { name: 'Denmark', id: 'Denmark' },
-    { name: 'Sweden', id: 'Sweden' },
-  ]) 
+  const [countries, setCountries] = useState([])
   const [newFilter, setNewFilter] = useState('');
+  const [countryToShow, setCountryToShow] = useState(''); //JOBBE MED DENNE VIDERE!
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://studies.cs.helsinki.fi/restcountries/api/all");
+        setCountries(response.data);
+      } catch (err) {
+        console.error(err); // Log the error for debugging
+      }
+    };
+
+    fetchData();
+  }, []);
 
   //Filter functionality
   const handleChange = (event) => {
@@ -15,26 +25,56 @@ const App = () => {
   };
 
   const filteredCountries = newFilter
-  ? countries.filter(
-    (country) =>
-      country.name.toLowerCase().includes(newFilter.toLowerCase())
-  )
-  : countries;
+    ? countries.filter(
+      (country) =>
+        country.name.common.toLowerCase().includes(newFilter.toLowerCase())
+    )
+    : countries;
 
+  //Show button
+  const showCountry = (country) => {
+
+    console.log(country.name.common)
+
+  };
 
   return (
     <div>
+
       <form>
         find countries: <input onChange={handleChange} />
       </form>
-      <ul>
-        {filteredCountries.map(country => 
-        <li key={country.id}> 
-        {country.name} 
-        </li>)}
-      </ul>
+
+      {filteredCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : filteredCountries.length === 1 ? (
+        
+      <div>
+        {filteredCountries.map(country => (
+          <div key={country.name.official}>
+            <h1>{country.name.common}</h1> 
+            <p>capital {country.capital}</p>
+            <p>area {country.area} km²</p>
+            <h2>languages:</h2> 
+            <li>{Object.values(country.languages).join(", ")}</li> 
+            <img src={country.flags.png} alt={`${country.name.common} flag`} />
+          </div>
+        ))}
+      </div>
+
+      ) : (
+        <ul>
+          {filteredCountries.map(country =>
+            <li key={country.name.official}>
+              {country.name.common} <button onClick={() => showCountry(country)}>show</button>
+            </li>
+          )}
+        </ul>
+      )
+      }
+
     </div>
-  )
+  );
 }
 
 export default App
