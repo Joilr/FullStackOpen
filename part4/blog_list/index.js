b289a524685f430fb9750ba6fc1e21486363b22b
Joilr <colamanx@protonmail.com>
2024-01-01 18:01:38 +0100

    4.2 (organized project)

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,61 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
-const Blog = require('./models/blog')
+const app = require('./app') // Import the Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
-app.use(morgan((tokens, req, res) => {
+const server = http.createServer(app)
 
-  const requestBody = JSON.stringify(req.body)
-
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'),
-    tokens['response-time'](req, res), 'ms',
-    `${requestBody}`
-  ].join(' ')
-}))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-//error handler
-const errorHandler = (error, request, response, next) => {
-  console.error('ERROR MESSAGE:' + error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-// this has to be the last loaded middleware.
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
