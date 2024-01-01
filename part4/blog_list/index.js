d8ded1cafd8e594941a8dd28ac7ef63eb42c4cae
Joilr <colamanx@protonmail.com>
2024-01-01 13:45:53 +0100

    4.1 + 4.2

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,22 +1,25 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const app = express()
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+
+  const requestBody = JSON.stringify(req.body)
+
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    tokens['response-time'](req, res), 'ms',
+    `${requestBody}`
+  ].join(' ')
+}))
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -36,7 +39,23 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+//error handler
+const errorHandler = (error, request, response, next) => {
+  console.error('ERROR MESSAGE:' + error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
