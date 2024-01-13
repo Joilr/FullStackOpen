29421fdc11c0014f151ff5fede0d5190a64791a4
Joilr <colamanx@protonmail.com>
2024-01-13 22:17:30 +0100

    5.6

---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
+
+  const addBlog = (event) => {
+    event.preventDefault()
+
+    createBlog ({
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url
+    })
+
+    setNewBlog({ title: '', author: '', url: '' })
+  }
+
+  return (
+    <div>
+      <h1>create new</h1>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input
+          value={newBlog.title}
+          onChange={(event) => setNewBlog({ ...newBlog, title: event.target.value })}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          value={newBlog.author}
+          onChange={(event) => setNewBlog({ ...newBlog, author: event.target.value })}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          value={newBlog.url}
+          onChange={(event) => setNewBlog({ ...newBlog, url: event.target.value })}
+        />
+      </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm
