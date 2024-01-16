f60eed07cc49975332e398896d64f9c412029296
Joilr <colamanx@protonmail.com>
2024-01-16 23:23:38 +0100

    6.1

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -9,16 +9,19 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
+
     const action = {
       type: 'DO_NOTHING'
     }
+    const state = initialState
 
+    deepFreeze(state)
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
 
   test('good is incremented', () => {
+
     const action = {
       type: 'GOOD'
     }
@@ -32,4 +35,50 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('bad is incremented', () => {
+
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('ok is incremented', () => {
+
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('zero', () => {
+
+    const action = {
+      type: 'ZERO is returned'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState);
+  })
+
+
 })
