d2304c471b111a7482da867a370fec9b1ded5eab
Joilr <colamanx@protonmail.com>
2024-02-24 23:43:20 +0100

    9.10

---
+++ part9/patientor/backend/src/services/diagnosesService.ts
@@ -0,0 +1,13 @@
+import diagnosesData from "../../data/diagnoses";
+
+import { DiagnosesEntry } from "../types";
+
+const diagnoses: DiagnosesEntry[] = diagnosesData;
+
+const getDiagnoses = (): DiagnosesEntry[] => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses,
+};
